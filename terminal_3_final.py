fe8c45a2454616eba948410361144cae4a65d0b4
haejun <bigmon021206@gmail.com>
2025-12-27 19:25:08 +0900

    gg

---
+++ terminal_3_final.py
@@ -0,0 +1,12 @@
+source /opt/ros/foxy/setup.bash
+source ~/Mobility_Challenge_Simulator/install/setup.bash
+export ROS_DOMAIN_ID=100
+
+cd ~/Mobility_Challenge_Simulator/tool
+
+# (중요) 혹시 남은 pub 있으면 죽이기
+pkill -f "ros2 topic pub"
+pkill -f "cav_waypoint_follower"
+
+python3 cav_waypoint_follower.py
+
