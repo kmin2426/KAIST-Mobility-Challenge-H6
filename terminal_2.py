d4fe66d0b436b6776c7602187c76854489602ffa
bigmon021206-bit <bigmon021206@gmail.com>
2026-01-02 22:04:57 +0900

    Delete terminal_2.py

--- terminal_2.py
+++
@@ -1,9 +0,0 @@
-#2. making path 
-
-cd Mobility_Challenge_Simulator/tool
-python3 make_full_path_21loop.py
-ls -al full_path_21loop.csv
-head -n 3 full_path_21loop.csv
-tail -n 3 full_path_21loop.csv
-
-
