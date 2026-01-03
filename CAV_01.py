6bbc12861c04f7bcd4c56deb7adc4ea67ba45c4a
chillguy007421 <minseo7421@gmail.com>
2026-01-03 16:38:11 +0900

    Update CAV_01.py

--- CAV_01.py
+++ CAV_01.py
@@ -9,7 +9,7 @@ import os
 
 
 # 파라미터 설정 
-PATH_FILENAME = "new_path.json"
+PATH_FILENAME = "path.py"
 LOOKAHEAD_DIST = 0.3   
 MAX_SPEED = 1.2        
 MIN_SPEED = 0.3        
@@ -169,3 +169,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
