4e1044e04b2bd69d58155cbaa01c48c29e42d286
chillguy007421 <minseo7421@gmail.com>
2026-01-03 16:04:54 +0900

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,5 +2,6 @@ cd ~/Mobility_Challenge_Simulator/tool
 source /opt/ros/foxy/setup.bash
 source ~/Mobility_Challenge_Simulator/install/setup.bash
 export ROS_DOMAIN_ID=100
-python3 control_code.py
+python3 CAV_01.py
+
 
